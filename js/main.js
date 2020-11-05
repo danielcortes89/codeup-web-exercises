1f3a69abba47928038bc43fb6f0dc0e82e51b836
Daniel Cortes <daniel.cortes.83944@gmail.com>
2020-11-05 16:53:42 -0600

    feat: Becoming a Javascript wiz

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict"
+console.log('Hello World');
+console.log("Hello from the other side");
+
+alert("You just won a new ipad")
+
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
