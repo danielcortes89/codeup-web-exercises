ef3fd32d35b08eb5c976145c193418084f085893
Daniel Cortes <daniel.cortes.83944@gmail.com>
2020-12-10 17:01:40 -0600

    feat: Exercise started

---
+++ js/mapbox_maps_api.js
@@ -0,0 +1,49 @@
+// 29.609374
+//
+// -98.598606
+
+
+// 0: -98.60634
+// 1: 29.609685
+
+
+// 0: -98.512909
+// 1: 29.620389
+
+mapboxgl.accessToken = mapboxToken;
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-98.598606, 29.609374],
+    zoom: 12
+});
+
+// let marker = new mapboxgl.Marker({
+//     color: "#FFF",
+//         draggable: true
+// }).setLngLat([-98.598606, 29.609374])
+//     .setZoom(12)
+//     .addTo(map);
+
+var marker = new mapboxgl.Marker()
+    .setLngLat([-98.598606, 29.609374])
+    .setPopup(new mapboxgl.Popup()
+        .setText("Latin")
+        .addTo(map))
+    .addTo(map);
+
+
+let ruth = new mapboxgl.Marker()
+    .setLngLat([-98.60634, 29.620389])
+    .setPopup(new mapboxgl.Popup()
+        .setText("Latin")
+        .addTo(map))
+    .addTo(map)
+
+
+
+// let popUp = new mapboxgl.Popup()
+//     .setLngLat([-98.598606, 29.609374])
+//     .setText("Latin")
+//     .addTo(map)
