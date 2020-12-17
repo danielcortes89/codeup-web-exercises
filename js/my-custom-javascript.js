c88d13adb9e1accdbe949791f828327e2cd91f50
Daniel Cortes <daniel.cortes.83944@gmail.com>
2020-12-17 15:00:04 -0600

    feat: mouse events

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,14 +1,49 @@
 "use strict"
 
+// $(document).ready(function(){
+//     alert("The Document is ready")
+// })
+
 // alert($('#second').html())
 
 // $('.codeup').css('border', '1px solid red')
 
-$('li').css('font-size', '20px')
-$('h1, li, p').html()
-$('p').html()
-alert(
-    // $('li').html()
-    // $('h1', 'h1', 'p').html()
-    $('h1, li, p').html()
+// $('li').css('font-size', '20px')
+// $('h1, li, p').html()
+// $('p').html()
+// alert(
+//     $('h1, li, p').html()
+// )
+
+
+// EVENTS
+// EVENTS
+
+// $(document).ready(function(){
+//     $('h1').click(
+//         function(){
+//             $(this).css('background', 'green')
+//         }
+//     )
+// })
+
+$('h1').click(
+    function(){
+        $(this).css('background', 'green')
+    }
+)
+
+$('p').dblclick(
+    function(){
+        $(this).css('font-size', '18px')
+    }
+)
+
+$('li').hover(
+    function(){
+        $(this).css('color', 'red')
+    },
+    function(){
+        $(this).css('color', 'black')
+    }
 )
