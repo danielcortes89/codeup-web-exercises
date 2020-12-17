1c1b186c49a4e5ac1beabb9ca65c4e2cea598c4f
Daniel Cortes <daniel.cortes.83944@gmail.com>
2020-12-17 14:07:35 -0600

    feat: Finished selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,4 +2,13 @@
 
 // alert($('#second').html())
 
-$('.codeup').css('border', '1px solid red')
+// $('.codeup').css('border', '1px solid red')
+
+$('li').css('font-size', '20px')
+$('h1, li, p').html()
+$('p').html()
+alert(
+    // $('li').html()
+    // $('h1', 'h1', 'p').html()
+    $('h1, li, p').html()
+)
